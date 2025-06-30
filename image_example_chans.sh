ba42f358ec57f5ab079f520a738a4ef05838dfa2
Tim Galvin <gal16b@petrichor-i3.cm.cluster>
2025-06-30 11:46:04 +1000

    added jolly to script

--- image_example_chans.sh
+++ image_example_chans.sh
@@ -1,15 +1,21 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash -;
 
 module load wsclean
 module load apptainer
+conda activate flint_main
 
+ORIGMS="/scratch3/gal16b/emu_download/uvw_test/scienceData.EMU_1141-55.SB47138.EMU_1141-55.beam00_averaged_cal.leakage.ms_trans"
 MS="scienceData.EMU_1141-55.SB47138.EMU_1141-55.beam00_averaged_cal.leakage.ms"
 AEGEAN="/scratch3/gal16b/containers/aegean.sif"
 DATA="DATA"
 CORRECT="TEST"
 
-echo "Will attempt wscleani, will use $DATA as column"
 
+echo "Making fresh copy of the data"
+rm -r "${MS}"
+cp -r "${ORIGMS}" "${MS}"
+
+echo "Will attempt wscleani, will use $DATA as column"
 wsclean \
    -j 32 \
    -size 7128 7128 \
@@ -32,7 +38,25 @@ wsclean \
    -name "all_${DATA}_column_subchan" \
    "${MS}"
 
+echo "Imaging finished. Running jolly, including modification ot flags"
 
+fix_ms_dir "${MS}"
+
+jolly_tractor \
+        tukey \
+        --chunk-size 2500 \
+        --data-column DATA \
+        --output-column TEST \
+        --overwrite \
+        --target-object sun \
+        --apply-towards-object \
+        --outer-width 0.5 \
+        --dry-run \
+        --ignore-nyquist-zone 1 \
+        "${MS}"
+
+
+echo "Running wsclean on modified data and flags"
 wsclean \
    -j 32 \
    -size 7128 7128 \
@@ -58,7 +82,7 @@ wsclean \
 for i in *image.fits
 do 
 	apptainer run $AEGEAN BANE --cores 4 --stripes 3 $i
-	apptainer run $AEGEAN aegean --maxsummit 5 --nocov --autoload "$i"; donfor i in *image.fits; do BANE --cores 4 --stripes 3 $i ;  aegean --maxsummit 5 --nocov --autoload "$i" 
+	apptainer run $AEGEAN aegean --maxsummit 5 --nocov --autoload "$i"
 done
 
 
